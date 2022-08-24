28c3619e2f25969583fcfbe7708ec23c059fd49c
AJSPARK31 <88500955+AJSPARK31@users.noreply.github.com>
2022-08-24 15:59:43 +0530

    Add files via upload

---
+++ WRITE A PYTHON PROGRAM TO SQUARE THE ELEMENTS OF A LIST USING MAP() FUNCTION. _.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Write a Python program to square the elements of a list using map() function.
+
+
+
+
+my_list=[4, 5, 2, 9]
+result=list(map(lambda x:x**2 , my_list))
+print(result)
+
+
+
+
+
+# In[ ]:
+
+
+
+
